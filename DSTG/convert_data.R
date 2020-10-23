42e7ef7ec995175905cb9dc9cf12392626903616
Qianqian Song <wasqqdyx@gmail.com>
2020-10-23 17:52:45 -0400

    Update convert_data.R

--- DSTG/convert_data.R
+++ DSTG/convert_data.R
@@ -5,11 +5,22 @@
 #' @examples: load data from folder "syntheic_data"
 
 source('R_utils.R')
-#' run sythetic data 
-synthetic.count <- readRDS('./synthetic_data/example_data.RDS')
-synthetic.label <- readRDS('./synthetic_data/example_label.RDS')
-
-Convert_Data(synthetic.count,synthetic.label,anova=FALSE)
-
 #' if you have the scRNA-seq data and want to decompose ST data, plese run below:
+
+if (length(args)==0) {
+    message('run sythetic data...')
+    synthetic.count <- readRDS('./synthetic_data/example_data.RDS')
+    synthetic.label <- readRDS('./synthetic_data/example_label.RDS')
+    Convert_Data(synthetic.count,synthetic.label,anova=FALSE)
+} else if (length(args)==3) {  
+    message('run real data...')
+    sc.count <- readRDS(args[1])
+    st.count <- readRDS(args[2])
+    intersect.genes <- intersect(rownames(sc.count),rownames(st.count))
+    sc.count <- sc.count[intersect.genes,]
+    st.count <- st.count[intersect.genes,]
+    count.list <- list(sc.count,st.count)
+    label.list <- list(data.frame(readRDS(args[3]),stringsAsFactors=F))
+    Convert_Data(count.list,label.list,anova=TRUE)
+}
 
